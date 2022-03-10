d2115975b39aabac2d34d5561267460db8ab784a
Naisargi25 <98091356+Naisargi25@users.noreply.github.com>
2022-03-10 22:00:25 +0530

    Create 4.c

---
+++ linux_assignments/4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+
+
+int main()
+{
+   int fd;
+   char read_buf[50];
+   char write_buf[50]="Hello How are you? How was your day?";
+   int len;
+   fd = open("linux_kernel.txt", O_CREAT | O_RDWR,0777); 
+   
+   len = write(fd,write_buf,50);
+
+   printf("Return value from a file is: %d\n",len); // return fd of file
+
+   lseek(fd,6,SEEK_SET); // To set file pointer to 6th byte/character in the file
+   read(fd,read_buf,len);
+   printf("The data from write buffer is: %s\n",read_buf);
+   close(fd);
+   return 0;
+}
