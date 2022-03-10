f44d72d144d8d52131d54f847e2478cb76a37cf6
Naisargi25 <98091356+Naisargi25@users.noreply.github.com>
2022-03-10 22:19:42 +0530

    Update 2.c

--- linux_assignments/2.c
+++ linux_assignments/2.c
@@ -11,14 +11,16 @@ int main()
    int len;
    char read_buf[50];
    fd = open("read.txt", O_CREAT | O_RDWR,0777);
+   
+   // At a time you can run only one lseek statement, you have to comment down other lseek statements
  
    lseek(fd,0,SEEK_SET); // To set file pointer to 0th byte/character in the file 
 
-   //lseek(fd,6,SEEK_SET); // To set file pointer to 6th byte/character in the file
+   lseek(fd,6,SEEK_SET); // To set file pointer to 6th byte/character in the file
 
-   //lseek(fd,-6,SEEK_END); // To find 3rd byte before the end of file
+   lseek(fd,-6,SEEK_END); // To find 3rd byte before the end of file
 
-   //lseek(fd,7,SEEK_CUR); // To find forward 7 bytes from current position
+   lseek(fd,7,SEEK_CUR); // To find forward 7 bytes from current position
 
    read(fd,read_buf,50);
    printf("The data from write buffer is: %s\n",read_buf);
