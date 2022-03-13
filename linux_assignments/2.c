e570f48d89c63333add5dbdd016524433b751d42
Naisargi25 <98091356+Naisargi25@users.noreply.github.com>
2022-03-13 15:31:47 +0530

    Update 2.c

--- linux_assignments/2.c
+++ linux_assignments/2.c
@@ -4,31 +4,31 @@
 
 int main()
 {	
-	char buff[100]="Hello, how are you?";
-	char readfile[100];
+    char buff[100]="Hello, how are you?";
+    char readfile[100];
 	
-	int f1;
+    int f1;
 
-   	//open a file called lseek.txt 
-	f1=open("lseek.txt",O_CREAT|O_RDWR,777);
+    //open a file called lseek.txt 
+    f1=open("lseek.txt",O_CREAT|O_RDWR,777);
 
-	write(f1,buff,50);
+    write(f1,buff,50);
 
-   	printf("File pointer set at start position of file\n");
-	lseek(f1,0,SEEK_SET);
-	read(f1,readfile,50);
-	printf("Data in file is: %s \n",readfile);
+    printf("File pointer set at start position of file\n");
+    lseek(f1,0,SEEK_SET);
+    read(f1,readfile,50);
+    printf("Data in file is: %s \n",readfile);
 
-	printf("\nFile pointer set at current position of file\n");
-	lseek(f1,-40,SEEK_CUR);
-	read(f1,readfile,50);
-	printf("Data in file is: %s \n",readfile);
+    printf("\nFile pointer set at current position of file\n");
+    lseek(f1,-40,SEEK_CUR);
+    read(f1,readfile,50);
+    printf("Data in file is: %s \n",readfile);
 
-	printf("\nFile pointer set at end position of file\n");
-	lseek(f1,-35,SEEK_END);
-	read(f1,readfile,50);
-	printf("Data in file is: %s \n",readfile);
+    printf("\nFile pointer set at end position of file\n");
+    lseek(f1,-35,SEEK_END);
+    read(f1,readfile,50);
+    printf("Data in file is: %s \n",readfile);
 
-	close(f1);
-   	return 0;
+    close(f1);
+    return 0;
 }
