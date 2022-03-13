86a9da670f0e9651606594cf204d3c7a6d46e105
Naisargi25 <98091356+Naisargi25@users.noreply.github.com>
2022-03-13 15:28:17 +0530

    Update 2.c

--- linux_assignments/2.c
+++ linux_assignments/2.c
@@ -4,17 +4,17 @@
 
 int main()
 {	
-   char buff[100]="Hello, how are you?";
+	char buff[100]="Hello, how are you?";
 	char readfile[100];
 	
 	int f1;
 
-   //open a file called lseek.txt 
+   	//open a file called lseek.txt 
 	f1=open("lseek.txt",O_CREAT|O_RDWR,777);
 
 	write(f1,buff,50);
 
-   printf("File pointer set at start position of file\n");
+   	printf("File pointer set at start position of file\n");
 	lseek(f1,0,SEEK_SET);
 	read(f1,readfile,50);
 	printf("Data in file is: %s \n",readfile);
@@ -30,5 +30,5 @@ int main()
 	printf("Data in file is: %s \n",readfile);
 
 	close(f1);
-   return 0;
+   	return 0;
 }
