63627d97e54c546366419b902f479cc9693237cd
Naisargi25 <98091356+Naisargi25@users.noreply.github.com>
2022-03-16 22:58:48 +0530

    Update 3.c

--- linux_assignments/3.c
+++ linux_assignments/3.c
@@ -9,18 +9,18 @@ int main( )
 {
 	struct stat st;
 	struct tm dt;
-  int fd;
+  	int fd;
 	
 	stat("5.c", &st);//
 	printf("File size = %lu\n",st.st_size); // size of file
 	printf("File inode = %lu\n", st.st_ino); // inode number of the file
 	printf("size disc of blocks = %lu\n",st.st_blksize); // size of disk of blocks
-  printf("No. of 512 byte blocks allocated = %d\n",st.st_blocks); 
+  	printf("No. of 512 byte blocks allocated = %d\n",st.st_blocks); 
 	printf("Permissions = %lo\n",st.st_mode); // file permissions
-  printf("Owner id = %lu\n",st.st_uid); // userid
-  printf("Group id = %lu\n",st.st_gid); // groupid
-  printf("Last modified time = %s\n",ctime(&st.st_mtime)); // last modified time
-  printf("Last accessed time = %s\n",ctime(&st.st_atime)); // last accessed time
+  	printf("Owner id = %lu\n",st.st_uid); // userid
+  	printf("Group id = %lu\n",st.st_gid); // groupid
+  	printf("Last modified time = %s\n",ctime(&st.st_mtime)); // last modified time
+  	printf("Last accessed time = %s\n",ctime(&st.st_atime)); // last accessed time
 	
 	printf("\n");
 	close(fd);
